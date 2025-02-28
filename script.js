1e10b448e1e16f5852c1c3e35217b96da6e6a4be
Azeb-H <azeb2haile@gmail.com>
2025-02-28 17:33:23 +0000

    add index, js and css

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Initialize Smooth Scroll
+    var scroll = new SmoothScroll('a[href*="#"]', {
+        speed: 800,
+        speedAsDuration: true
+    });
+});
+
